f121e6967582151f86da5210ea4cf143ae0bf7d9
Denis <wudmccpa@gmail.com>
2021-09-08 10:30:26 +0300

    add new beget hosts to settings.py

--- rentsproject/settings.py
+++ rentsproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+i!peshs*h++1!qi5fp!z*(m81vmx225#1f^9w%lw&w#rophzd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mironok8.beget.tech', 'www.mironok8.beget.tech']
+ALLOWED_HOSTS = ['mironok8.beget.tech', 'www.mironok8.beget.tech', '127.0.0.1']
 
 
 # Application definition
